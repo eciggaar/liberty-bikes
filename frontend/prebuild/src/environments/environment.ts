9fd9da06004d12db00e9906f7cf0304f03e0114e
Edward Ciggaar <edward_ciggaar@nl.ibm.com>
2019-04-09 16:30:45 +0000

    Changed description and modified service URLs to reflect custom nodePorts

--- frontend/prebuild/src/environments/environment.ts
+++ frontend/prebuild/src/environments/environment.ts
@@ -15,10 +15,10 @@
 
 export const environment = {
   production: false,
-  API_URL_AUTH: `http://${document.location.hostname}:32331`,
-  API_URL_PARTY: `http://${document.location.hostname}:30707/party`,
-  API_URL_GAME_ROUND: `http://${document.location.hostname}:30707/round`,
-  API_URL_GAME_WS: `ws://${document.location.hostname}:30707/round/ws`,
-  API_URL_PLAYERS: `http://${document.location.hostname}:30714/player`,
-  API_URL_RANKS: `http://${document.location.hostname}:30714/rank`
+  API_URL_AUTH: `http://${document.location.hostname}:308082`,
+  API_URL_PARTY: `http://${document.location.hostname}:308080/party`,
+  API_URL_GAME_ROUND: `http://${document.location.hostname}:308080/round`,
+  API_URL_GAME_WS: `ws://${document.location.hostname}:308080/round/ws`,
+  API_URL_PLAYERS: `http://${document.location.hostname}:308081/player`,
+  API_URL_RANKS: `http://${document.location.hostname}:308081/rank`
 };
