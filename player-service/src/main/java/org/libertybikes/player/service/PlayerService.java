6a518d16391ba856a14eea0d136e2b8329744d51
Edward Ciggaar <edward_ciggaar@nl.ibm.com>
2019-04-12 09:33:43 +0000

    Added new method to player service

--- player-service/src/main/java/org/libertybikes/player/service/PlayerService.java
+++ player-service/src/main/java/org/libertybikes/player/service/PlayerService.java
@@ -80,4 +80,14 @@ public class PlayerService {
         map.put("id", id);
         return map;
     }
+    
+    @GET
+    @Path("/getTestString")
+    public HashMap<String, String> getTestString() {
+    	
+    	HashMap<String, String> map = new HashMap<String, String>();
+    	map.put("result","test string");
+    	
+    	return map;
+    }
 }
